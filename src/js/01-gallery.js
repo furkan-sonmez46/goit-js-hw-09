8dc3696b0530abff73e6cd97352faf780fca99b6
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-01-15 23:35:20 +0300

    gallery dosyaları bağlandı ancak açılmıyor

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+import { images } from '../data/images';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMap = images
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join('');
+
+gallery.innerHTML = galleryMap;
